c6df67c6d4de71b0e69b35207b766cd98aa48a6f
Hyukjoon Kwon <just.hjkwon@gmail.com>
2020-07-10 19:59:25 +0900

    [#2] Add Image class.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,11 @@
 #include <opencv2/highgui.hpp>
+#include "image.h"
+
 
 int main (int argc, char *argv[]) {
+    cv::Mat input_image = cv::imread("Lenna.png");
+
+    Image image = Image::FromMat(input_image);
+
     return 0;
 }
