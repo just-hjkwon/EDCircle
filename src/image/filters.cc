97cd18cb6be13f7753ec351c2a4a6d50fb562e45
Hyukjoon Kwon <just.hjkwon@gmail.com>
2020-07-11 00:29:02 +0900

    [#3] Switch filter direction.

--- src/image/filters.cc
+++ src/image/filters.cc
@@ -33,18 +33,18 @@ Filter FilterFactory::CreateGaussianFilter(std::size_t filter_size,
 }
 
 Filter FilterFactory::PrewittXFilter() {
-  std::vector<float> buffer = { 1.0f,  0.0f, -1.0f, 
-                                1.0f,  0.0f, -1.0f, 
-                                1.0f,  0.0f,  -1.0f};
+  std::vector<float> buffer = { -1.0f,  0.0f, 1.0f, 
+                                -1.0f,  0.0f, 1.0f, 
+                                -1.0f,  0.0f, 1.0f};
 
   const std::size_t filter_size = 3;
   return Filter(filter_size, buffer.data());
 }
 
 Filter FilterFactory::PrewittYFilter() {
-  std::vector<float> buffer = { 1.0f,  1.0f, 1.0f,
+  std::vector<float> buffer = { -1.0f,  -1.0f, -1.0f,
                                 0.0f, 0.0f, 0.0f,
-                               -1.0f, -1.0f, -1.0f };
+                               1.0f, 1.0f, 1.0f };
 
   const std::size_t filter_size = 3;
   return Filter(filter_size, buffer.data());
