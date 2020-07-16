16c5251db9dfcf63c9a1013bdacd386efe55dbf6
Hyukjoon Kwon <just.hjkwon@gmail.com>
2020-07-17 03:18:48 +0900

    [#6] Parse arguments and do process depends on that.

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,5 @@
+#include "main.h"
+
 #include <chrono>
 #include <iostream>
 #include <opencv2/highgui.hpp>
@@ -11,9 +13,100 @@
 #include "image/gray_image.h"
 #include "primitives/circle.h"
 
+struct Config {
+ public:
+  std::string filename;
+  bool video_mode;
+  bool error;
+};
+
+void print_help();
+void print_invalid_input_file(std::string filename);
+Config parse_args(int argc, char *argv[]);
+void DetectCircle(cv::Mat &cv_image);
+
 int main(int argc, char *argv[]) {
-  cv::Mat input_image = cv::imread("Lenna.png", cv::IMREAD_GRAYSCALE);
-  GrayImage image = GrayImage::FromMat(input_image);
+  Config config = parse_args(argc, argv);
+  if (config.error == true) {
+    print_help();
+    return -1;
+  }
+
+  if (config.video_mode == true) {
+    cv::VideoCapture video;
+    bool is_opened = video.open(config.filename);
+
+    if (is_opened == false) {
+      print_invalid_input_file(config.filename);
+      return -1;
+    }
+
+    while (true) {
+      cv::Mat frame;
+      video.read(frame);
+      if (frame.empty() == true) {
+        break;
+      }
+
+      DetectCircle(frame);
+
+      cv::imshow("EVideo", frame);
+      char pressed_key = cv::waitKey(1);
+      if (pressed_key == 'q') {
+        break;
+      }
+    }
+  } else {
+    cv::Mat image = cv::imread(config.filename);
+    if (image.empty() == true) {
+      print_invalid_input_file(config.filename);
+      return -1;
+    }
+
+    DetectCircle(image);
+
+    cv::imshow("Image", image);
+    cv::waitKey(0);
+  }
+}
+
+void print_help() {
+  std::cout << "Usage: EDCircle [-v|-i] [video filename|image filename]"
+            << std::endl;
+}
+
+void print_invalid_input_file(std::string filename) {
+  std::cout << "Invalid input file: " << filename << std::endl;
+}
+
+Config parse_args(int argc, char *argv[]) {
+  if (argc != 3) {
+    return Config{"", false, true};
+  }
+
+  bool video_mode = false;
+  if (std::string("-v").compare(argv[1]) == 0) {
+    video_mode = true;
+  } else if (std::string("-i").compare(argv[1]) == 0) {
+    video_mode = false;
+  } else {
+    return Config{"", false, true};
+  }
+
+  std::string filename(argv[2]);
+
+  return Config{filename, video_mode, false};
+}
+
+void DetectCircle(cv::Mat &cv_image) {
+  cv::Mat cv_gray_image;
+  if (cv_image.type() == CV_8UC3) {
+    cv::cvtColor(cv_image, cv_gray_image, cv::COLOR_BGR2GRAY);
+  } else {
+    cv_gray_image = cv_image;
+  }
+
+  GrayImage image = GrayImage::FromMat(cv_gray_image);
 
   std::chrono::system_clock::time_point start;
   std::chrono::duration<double> sec;
@@ -31,20 +124,4 @@ int main(int argc, char *argv[]) {
   sec = std::chrono::system_clock::now() - start;
   std::cout << "DetectCircle Elapsed time: " << sec.count() * 1000.0f << " ms"
             << std::endl;
-
-  cv::Mat i;
-  cv::cvtColor(input_image, i, cv::COLOR_GRAY2BGR);
-
-  for (auto circle : ed_circle.circles_) {
-    circle.Draw(i, cv::Scalar(0, 0, 255));
-  }
-
-  for (auto e : ed_circle.ellipses_) {
-    e.Draw(i, cv::Scalar(0, 255, 255));
-  }
-
-  cv::imshow("i", i);
-  cv::waitKey(0);
-
-  return 0;
 }
